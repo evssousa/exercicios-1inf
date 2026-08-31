9629c8d282f68fb5f64e5eef33cb50bc51ca45dc
Vagner Borge <francisco.soares457@aluno.ce.gov.br>
2026-08-31 23:40:58 +0000

    solução da atvidade

--- 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
+++ 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
@@ -1,7 +1,41 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
+const cliente = "Mariana Silva"
+const cidade = "Fortaleza"
+const produto = "Notebook Pro"
+const categoria = "Notebook"
+const quantidade = 2
+const preco = 3500 
+const subtotal = preco * quantidade
+const descontoPercentual = 10
+const valorDesconto = (subtotal * descontoPercentual)/100
+const valorFinal = subtotal - valorDesconto
+const valorPago = 7000
+const troco = valorPago - valorFinal
 
 
+const resumo = (`==================================================
+TECHSTORE SISTEMAS
+RESUMO DO PEDIDO
+==================================================
 
+cliente: ${cliente}
+cidade:  ${cidade}
+
+produto: ${produto}
+categoria: ${categoria}
+quantidade: ${quantidade}
+preço unitário: ${preco}
+subtotal: R$ ${subtotal}
+desconto: ${descontoPercentual}%
+valorDesconto: ${valorDesconto}
+valor final: R$ ${valorFinal}
+
+valor pago: R$ ${valorPago}
+troco: R$ ${troco}
+
+Obrigado pela compra!
+==================================================`)
+console.log(resumo)
 
 
 
